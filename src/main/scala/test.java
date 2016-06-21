836e12f297c0c58b3a6fdad7a0ab81a8ee9613a8
didi <zhangshoudi@didichuxing.com>
2016-06-21 13:23:36 +0800

    test code

--- src/main/scala/test.java
+++ src/main/scala/test.java
@@ -1,26 +1,24 @@
-import jdk.nashorn.internal.runtime.ArgumentSetter;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
 
 /**
  * Created by didi on 16/6/21.
  */
 
-import java.sql.{Date, Timestamp}
 
-import scala.collection.JavaConverters._
-
-import org.apache.spark.sql._
-import org.apache.spark.sql.catalyst.{TableIdentifier, DefaultParserDialect}
-import org.apache.spark.sql.catalyst.analysis.{FunctionRegistry, EliminateSubQueries}
-import org.apache.spark.sql.catalyst.errors.DialectException
-import org.apache.spark.sql.execution.datasources.LogicalRelation
-import org.apache.spark.sql.hive.test.TestHiveSingleton
-import org.apache.spark.sql.hive.{HiveContext, HiveQLDialect, MetastoreRelation}
-import org.apache.spark.sql.execution.datasources.parquet.ParquetRelation
-import org.apache.spark.sql.test.SQLTestUtils
-import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.types.CalendarInterval
 
 public class test {
-    public void main(ArgumentSetter)
+    public void main( String[] args){
+        SparkConf conf=new SparkConf().setAppName("name01");
+        JavaSparkContext sc=new JavaSparkContext(conf);
+        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
+        DataFrame df=sqlContext.sql(args[0].toString());
+        df.repartition(1000);
+        df.write();
+
+
+    }
 
 }
